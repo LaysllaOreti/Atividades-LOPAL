6acf9b3c300ab20900b02df7a4d1c81aa38355cd
Layslla Eduarda Oreti dos Santos <116527092+LaysllaOreti@users.noreply.github.com>
2025-04-11 16:28:23 -0300

    Update Exercicio8.py

--- Lista Laço de Repetição/Exercicio8.py
+++ Lista Laço de Repetição/Exercicio8.py
@@ -1,24 +1,13 @@
 import random
 
-def lancar_moeda(): #função que simula o lançamento de uma moeda
-    return random.choice(["cara", "coroa"])
+i = 0 #inicializa como 0
+escolha = []
 
-#inicializando as variáveis
-sequencia = []
-contador_cara = 0
-
-#simula o lançamento
-while contador_cara < 3:
-    resultado = lancar_moeda()
-    print(f"Lançamento: {resultado}")
-    
-    #adiciona o resultado no final da sequência
-    sequencia.append(resultado)
-    
-    #verifica se as últimas 3 jogadas foram "cara"
-    if len(sequencia) >= 3 and sequencia[-3:] == ["cara", "cara", "cara"]:
-        contador_cara = 3  #encerrar o loop
+while i < 3:
+    operacao = random.choice(["cara", "coroa"])
+    escolha.append(operacao)
+    if operacao == "cara":
+        i += 1
     else:
-        contador_cara = sequencia[-3:].count("cara")  #conta o número de "caras" consecutivas
-
-print("Sequência final de lançamentos:", sequencia)
+        continue
+    print(escolha)
