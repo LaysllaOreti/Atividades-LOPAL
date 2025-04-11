98c0d41f0ad3c680b7e0357f16478fe0afc23f1b
Layslla Eduarda Oreti dos Santos <116527092+LaysllaOreti@users.noreply.github.com>
2025-04-11 16:25:26 -0300

    Update Exercício10.py

--- Lista Laço de Repetição/Exercício10.py
+++ Lista Laço de Repetição/Exercício10.py
@@ -1,27 +1,8 @@
-#inicializa as variáveis
-maior = None
-segundo_maior = None
+lista = []
+i = 0 #inicializa como 0
 
-while True:
-    numero = input("Digite um número (ou 'sair' para terminar): ")
-    
-    if numero.lower() == 'sair':
-        break
-    
-    try:
-        numero = int(numero)
-        
-        #verifica o maior e segundo maior números
-        if maior is None or numero > maior:
-            segundo_maior = maior
-            maior = numero
-        elif segundo_maior is None or numero > segundo_maior:
-            segundo_maior = numero
-    except ValueError:
-        print("Por favor, digite um número válido.")
+for i in range(1,11):
+    lista.append(i)
+numero_maior = lista.remove(max(lista))
 
-#exibe o segundo maior número
-if segundo_maior is not None:
-    print(f"O segundo maior número é: {segundo_maior}")
-else:
-    print("Não há segundo maior número. Foi digitado apenas um número.")
+print(f"O segundo maior número é: {max(lista)}")
